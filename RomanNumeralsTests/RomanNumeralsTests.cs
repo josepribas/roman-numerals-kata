451f0dfa767747cab85bdb87fca342c1bcb26b9c
joseplluis <jlribas@girosystem.com>
2024-01-31 21:58:12 +0100

    Add tests for 7, 18, 76 and 1688

--- RomanNumeralsTests/RomanNumeralsTests.cs
+++ RomanNumeralsTests/RomanNumeralsTests.cs
@@ -11,11 +11,15 @@ public class Arabic2RomanNumeral
     [InlineData("III", 3)]
     [InlineData("V", 5)]
     [InlineData("VI", 6)]
+    [InlineData("VII", 7)]
     [InlineData("X", 10)]
+    [InlineData("XVIII", 18)]
     [InlineData("L", 50)]
+    [InlineData("LXXVI", 76)]
     [InlineData("C", 100)]
     [InlineData("D", 500)]
     [InlineData("M", 1000)]
+    [InlineData("MDCLXXXVIII", 1688)]
     public void ShouldConvertCorrectly(string expected, int arabic)
     {
         Arabic2RomanNumeralConverter.Convert(arabic).Should().Be(expected);       
