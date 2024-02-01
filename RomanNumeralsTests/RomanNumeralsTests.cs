6c35bd737520af3cdcf988a1b7fa0670554cf3c3
joseplluis <jlribas@girosystem.com>
2024-02-01 21:41:59 +0100

    Add test: 18 = XVIII

--- RomanNumeralsTests/RomanNumeralsTests.cs
+++ RomanNumeralsTests/RomanNumeralsTests.cs
@@ -12,6 +12,7 @@ public class Arabic2RomanNumeral
     [InlineData("V", 5)]
     [InlineData("VII", 7)]
     [InlineData("X", 10)]
+    [InlineData("XVIII", 18)]
     public void ShouldConvertCorrectly(string expected, int arabic)
     {
         Arabic2RomanNumeralConverter.Convert(arabic).Should().Be(expected);       
