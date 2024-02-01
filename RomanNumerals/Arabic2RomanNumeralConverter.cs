3fb28e83703d2cd5adf525c1b69f4204c7c94f7a
joseplluis <jlribas@girosystem.com>
2024-02-01 21:54:09 +0100

    Add tests for 4, 9, 40, 90, 400 and 900

--- RomanNumerals/Arabic2RomanNumeralConverter.cs
+++ RomanNumerals/Arabic2RomanNumeralConverter.cs
@@ -7,11 +7,17 @@ public class Arabic2RomanNumeralConverter
     private static Dictionary<int, string> conversions =
         new Dictionary<int, string>() {                        
             { 1000, "M"},
+            { 900, "CM"},
             { 500, "D"},
+            { 400, "CD"},
             { 100, "C"},
+            { 90, "XC"},
             { 50, "L"},
+            { 40, "XL"},
             { 10, "X"},
+            { 9, "IX"},
             { 5, "V"},
+            { 4, "IV"},
             { 1, "I"},
         };
 
